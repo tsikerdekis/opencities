7fe3f4b6fe24f6a49d146910a1d8014afabfa839
unknown <tsikerdekis@gmail.com>
2016-07-20 21:16:09 -0400

    allfiles

---
+++ Source/OpenCities/OpenCitiesGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "OpenCitiesGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENCITIES_API AOpenCitiesGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
